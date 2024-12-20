7843a61268c2cf9db7a86c61cc36eb663ea744fb
Caryn Carter <carter.caryn@gmail.com>
2024-12-19 18:07:28 -0600

    modified signup form for ADD_USER mutation

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-//import { useMutation } from '@apollo/client'; // Import useMutation from Apollo Client
-//import { ADD_USER } from '../utils/mutations'; // Import ADD_USER mutation
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client'; 
+import { ADD_USER } from '../utils/mutations'; 
+//import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 import type { User } from '../models/User';
 
@@ -15,6 +15,9 @@ const SignupForm = ({}: { handleModalClose: () => void }) => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+    // Set up mutation hook
+    const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -32,14 +35,23 @@ const SignupForm = ({}: { handleModalClose: () => void }) => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: {
+          input: {
+            username: userFormData.username,
+            email: userFormData.email,
+            password: userFormData.password,
+          },
+        },
+      });
 
-      if (!response.ok) {
+      if (!data) {
         throw new Error('something went wrong!');
       }
 
-      const { token } = await response.json();
+      const { token } = data.addUser;
       Auth.login(token);
+
     } catch (err) {
       console.error(err);
       setShowAlert(true);
